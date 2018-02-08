aec36fa65d82c96cd08e6e7318da4440c5e1a049
Craig Mathieson <craigsskiingyo@gmail.com>
2018-02-08 14:18:15 -0800

    abstracted functions

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
+import makeEnemies from from "init.js"
+var GameBoard = require('./init.js').GameBoard
+var Snake = require('./init.js').Snake
+
 var express = require('express')
 var router  = express.Router()
 
@@ -36,102 +40,10 @@ router.post('/move', function (req, res) {
   // Response data
   var data = {
     move: 'right', // one of: ['up','down','left','right']
-    taunt: 'Outta my way, snake!', // optional, but encouraged!
+    taunt: 'moar snep bek plz', // optional, but encouraged!
   }
 
   return res.json(data)
 })
-
-//builds a snake object
-function Snake(name, length, id, health, bodys) {
-  this.name = name;
-  this.length = length;
-  this.id = id;
-  this.health = health;
-
-  this.body = [];
-  for(var i = 0; i < Object.keys(bodys.data).length; i++) {
-    this.body.push(bodys.data[i]);
-  }
-}
-
-//builds the gameboard object
-function GameBoard(id, height, width, turn, foods) {
-  this.id = id;
-  this.height = height;
-  this.width = width;
-  this.turn = turn;
-
-  this.food = [];
-  for(var j = 0; j < Object.keys(foods.data).length; j++) {
-    this.food.push(foods.data[j]);
-  }
-}
-
-//builds enemy snake array
-function makeEnemies(mySnake, input) {
-  var enemies = new Array();
-
-  for(var p = 0; p < Object.keys(input.snakes.data).length; p++) {
-
-    var snek = new Snake(input.snakes.data[p].name, input.snakes.data[p].length, input.snakes.data[p].id, input.snakes.data[p].health, input.snakes.data[p].body);
-
-    if(JSON.stringify(snek) === JSON.stringify(mySnake)) {
-      continue;
-    } else {
-      enemies.push(snek);
-    }
-  }
-
-  return enemies;
-}
-
-/*function printStats(input) {
-
-  //will use this to pull game stats
-
-  var yoSnakeBod = Object.keys(input.you.body.data);
-  var allSnakes = Object.keys(input.snakes.data);
-  var allFood = Object.keys(input.food.data);
-
-  //console.log("My Snake:");
-  //console.log("name: " + input.you.name);
-  //console.log("length: " + input.you.length);
-  //console.log("snake id: " + input.you.id);
-  //console.log("health: " + input.you.health);
-  //for(var i = 0; i < yoSnakeBod.length; i++){
-  //  console.log("Body Part #" + (i+1) + ": " + input.you.body.data[i].x + " " + input.you.body.data[i].y);
-  //}
-  //console.log();
-
-  //console.log("Board Stats:");
-  //console.log("Game id: " + input.id);
-  //console.log("width: " + input.width);
-  //console.log("height: " + input.height);
-  //console.log("turn: " + input.turn);
-  //console.log();
-
-  console.log("Snake List:");
-
-  for(var k = 0; k < allSnakes.length; k++){
-    console.log("snake #" + (k+1));
-    console.log("health: " + input.snakes.data[k].health);
-    console.log("id: " + input.snakes.data[k].id);
-    console.log("length: " + input.snakes.data[k].length);
-    console.log("name: " + input.snakes.data[k].name);
-    var thisSnakeBod = Object.keys(input.snakes.data[k].body.data);
-    for(var j = 0; j < thisSnakeBod.length; j++){
-      console.log("Body Part #" + (j+1) + ": " + input.snakes.data[k].body.data[j].x + " " + input.snakes.data[k].body.data[j].y);
-    }
-    console.log();
-  }
-
-  //console.log();
-
-  //console.log("Food Locations:");
-  //for(var p = 0; p < allFood.length; p++) {
-  //  console.log("Food item #" + (p+1) + ": " + input.food.data[p].x + " " + input.food.data[p].y);
-  //}
-}*/
 
 module.exports = router
---
+++ routes/init.js
@@ -0,0 +1,47 @@
+//builds a snake object
+function Snake(name, length, id, health, bodys) {
+  this.name = name;
+  this.length = length;
+  this.id = id;
+  this.health = health;
+
+  this.body = [];
+  for(var i = 0; i < Object.keys(bodys.data).length; i++) {
+    this.body.push(bodys.data[i]);
+  }
+}
+
+//builds enemy snake array
+function makeEnemies(mySnake, input) {
+  var enemies = new Array();
+
+  for(var p = 0; p < Object.keys(input.snakes.data).length; p++) {
+
+    var snek = new Snake(input.snakes.data[p].name, input.snakes.data[p].length, input.snakes.data[p].id, input.snakes.data[p].health, input.snakes.data[p].body);
+
+    if(JSON.stringify(snek) === JSON.stringify(mySnake)) {
+      continue;
+    } else {
+      enemies.push(snek);
+    }
+  }
+
+  return enemies;
+}
+
+//builds the gameboard object
+function GameBoard(id, height, width, turn, foods) {
+  this.id = id;
+  this.height = height;
+  this.width = width;
+  this.turn = turn;
+
+  this.food = [];
+  for(var j = 0; j < Object.keys(foods.data).length; j++) {
+    this.food.push(foods.data[j]);
+  }
+}
+
+export default makeEnemies;
+exports.Snake = Snake;
+exports.GameBoard = GameBoard;
