02b86dfe72e21c23ddd7e9fca924f154cc2f25b8
Craig Mathieson <craigsskiingyo@gmail.com>
2018-02-08 14:32:09 -0800

    abstracted functions

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,3 @@
-import makeEnemies from "init.js"
-var GameBoard = require('./init.js');
-var Snake = require('./init.js');
-
 var express = require('express')
 var router  = express.Router()
 
@@ -45,5 +41,49 @@ router.post('/move', function (req, res) {
 
   return res.json(data)
 })
+
+//builds a snake object
+function Snake(name, length, id, health, bodys) {
+  this.name = name;
+  this.length = length;
+  this.id = id;
+  this.health = health;
+
+  this.body = [];
+  for(var i = 0; i < Object.keys(bodys.data).length; i++) {
+    this.body.push(bodys.data[i]);
+  }
+}
+
+//builds enemy snake array
+function makeEnemies(mySnake, input) {
+  var enemies = new Array();
+
+  for(var p = 0; p < Object.keys(input.snakes.data).length; p++) {
+
+    var snek = new Snake(input.snakes.data[p].name, input.snakes.data[p].length, input.snakes.data[p].id, input.snakes.data[p].health, input.snakes.data[p].body);
+
+    if(JSON.stringify(snek) === JSON.stringify(mySnake)) {
+      continue;
+    } else {
+      enemies.push(snek);
+    }
+  }
+
+  return enemies;
+}
+
+//builds the gameboard object
+function GameBoard(id, height, width, turn, foods) {
+  this.id = id;
+  this.height = height;
+  this.width = width;
+  this.turn = turn;
+
+  this.food = [];
+  for(var j = 0; j < Object.keys(foods.data).length; j++) {
+    this.food.push(foods.data[j]);
+  }
+}
 
 module.exports = router
