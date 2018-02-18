e083a2cdea246ac2cf3a41db858f5e2af0b9c0e6
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-02-18 15:41:42 -0800

    change direction

--- routes/gameBoard.js
+++ routes/gameBoard.js
@@ -7,12 +7,12 @@ module.exports = exports = function (board, mySnake, enemies) {
 
 	// Empty space = 0 on the board
 	// Walls = 5
-	for(var i = 0; i < (board.width+1); i++) {
+	for(var i = 0; i < (board.width+2); i++) {
 		arr.push([]);
-		arr[i].push( new Array(board.height+1));
+		arr[i].push( new Array(board.height+2));
 
-		for(var j = 0; j < (board.height+1); j++){
-			if(i == board.width || i == 0 || j == 0 || j == board.height){
+		for(var j = 0; j < (board.height+2); j++){
+			if(i == board.width+1 || i == 0 || j == 0 || j == board.height+1){
 				arr[i][j] = 5;
 			} else {
 				arr[i][j] = 0;
@@ -22,7 +22,7 @@ module.exports = exports = function (board, mySnake, enemies) {
 
 	// Food = 1 on the board
 	for(var i = 0; i < board.food.length; i++) {
-		arr[board.food[i].y][board.food[i].x] = 1;
+		arr[board.food[i].y+1][board.food[i].x+1] = 1;
 	}
 
 	// Enemy body parts 
@@ -30,17 +30,17 @@ module.exports = exports = function (board, mySnake, enemies) {
 		for(var j = 0; j < enemies[i].body.length; j++) {
 			if(j==0) {
 				// head = 3
-				arr[enemies[i].body[j].y][enemies[i].body[j].x] = 3;
+				arr[enemies[i].body[j].y+1][enemies[i].body[j].x+1] = 3;
 			} else {
 				// body = 2
-				arr[enemies[i].body[j].y][enemies[i].body[j].x] = 2;
+				arr[enemies[i].body[j].y+1][enemies[i].body[j].x+1] = 2;
 			}
 		}
 	}
 
 	// friendly body parts = 4;
 	for(var i = 0; i < mySnake.body.length; i++) {
-		arr[mySnake.body[i].y][mySnake.body[i].x] = 4;
+		arr[mySnake.body[i].y+1][mySnake.body[i].x+1] = 4;
 	}
 
 
