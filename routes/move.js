c7aadd62e2c617e8bc2114507b4e39c119a6fdd7
Craig Mathieson <craigmathieson@w134-87-181-035.wireless.uvic.ca>
2018-02-19 19:58:01 -0800

    abstracted cheeckWalls

--- routes/move.js
+++ routes/move.js
@@ -3,13 +3,24 @@ var checkWalls = require('./checkWalls.js')
 module.exports = exports = function (mySnake, enemies, board) {
 	var decision = 'up'; //default move
 
-
+	decision = findFood(mySnake, enemies, board);
 
 	if(mySnake.body[0].x == 0 || mySnake.body[0].x == board.width || mySnake.body[0].y == 0 || mySnake.body[0].y == board.height){
 		decision = checkWalls(mySnake, board, decision);
 	}
 	//returns 'left', 'right', 'up', 'down'
 	return decision;
+}
+
+var findFood = function(mySnake, enemies, board) {
+	var topFoods = [];
+	for(var i = 0; i < board.food.length; i++) {
+		if((Math.abs(mySnake.body[0].x - board.food[i].x)/(board.width+1)) + (Math.abs(mySnake.body[0].y - board.food[i].y)/(board.height+1) <= 1) {
+			topFoods.push(board.food[i]);
+		}
+	}
+	console.log(topFoods);
+	return 'down';
 }
 
 var contains = function (list, x, y) {
