f843d735d78725dc0ae72d4f6969f27207ae98b4
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-02-20 14:12:46 -0800

    added basic food finding function

--- routes/checkWalls.js
+++ routes/checkWalls.js
@@ -4,7 +4,6 @@
 module.exports = exports = function (mySnake, board, original, decision)  {
 	//top left corner
 	if(mySnake.body[0].x == 0 && mySnake.body[0].y == 0){
-		console.log("top left!");
 		if(contains(mySnake.body, 1, 0)) {
 			decision.down += 99999;
 		} else {
@@ -13,7 +12,6 @@ module.exports = exports = function (mySnake, board, original, decision)  {
 	}
 	//bottom left corner
 	if(mySnake.body[0].x == 0 && mySnake.body[0].y == board.height) {
-		console.log("bottom left!");
 		if(contains(mySnake.body, 0, board.height-1)) {
 			decision.right += 99999;
 		} else {
@@ -22,7 +20,6 @@ module.exports = exports = function (mySnake, board, original, decision)  {
 	}
 	//top right corner
 	if(mySnake.body[0].x == board.width && mySnake.body[0].y == 0) {
-		console.log("top right!");
 		if(contains(mySnake.body, board.width-1, 0)) {
 			decision.down += 99999;
 		} else {
@@ -31,7 +28,6 @@ module.exports = exports = function (mySnake, board, original, decision)  {
 	}
 	//bottom right corner
 	if(mySnake.body[0].x == board.width && mySnake.body[0].y == board.height) {
-		console.log("bottom right!");
 		if(contains(mySnake.body, board.width, board.height-1)) {
 			decision.left += 99999;
 		} else {
@@ -41,35 +37,19 @@ module.exports = exports = function (mySnake, board, original, decision)  {
 
 	//left side
 	if(mySnake.body[0].x == 0) {
-		console.log("left side!");
-		// if |<--
-		if(original === 'left') {
-			decision.left -= 99999;
-		}
+		decision.left -= 99999;
 	}
 	// right side
 	if(mySnake.body[0].x == board.width) {
-		console.log("right side!");
-		// -->|
-		if(original === 'right') {
-			decision.right -= 99999;
-		}
+		decision.right -= 99999;
 	}
 	// top
 	if(mySnake.body[0].y == 0) {
-		console.log("at top!");
-		// ^
-		if(original === 'up') {
-			decision.up -= 99999;
-		}
+		decision.up -= 99999;
 	}
 	//bottom
 	if(mySnake.body[0].y == board.height) {
-		console.log("at bottom!");
-		// v
-		if(original === 'down') {
 			decision.down -= 99999;
-		}
 	}
 	return;
 }
