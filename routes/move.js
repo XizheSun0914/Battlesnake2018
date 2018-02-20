20038dddc8ff7ea179d1cdb6f47cebf88bd634b3
Craig Mathieson <craigmathieson@w134-87-181-035.wireless.uvic.ca>
2018-02-19 19:18:52 -0800

    abstracted cheeckWalls

--- routes/move.js
+++ routes/move.js
@@ -1,14 +1,14 @@
 var checkWalls = require('./checkWalls.js')
 
 module.exports = exports = function (mySnake, enemies, board) {
-	var decision = 'up';
-	//var board = findValues(currentBoard);
+	var decision = 'up'; //default move
+
+
 
 	if(mySnake.body[0].x == 0 || mySnake.body[0].x == board.width || mySnake.body[0].y == 0 || mySnake.body[0].y == board.height){
 		decision = checkWalls(mySnake, board, decision);
 	}
-
-	//return 'left', 'right', 'up', 'down'
+	//returns 'left', 'right', 'up', 'down'
 	return decision;
 }
 
@@ -19,4 +19,4 @@ var contains = function (list, x, y) {
 		}
 	}
 	return false;
-} 
+}
