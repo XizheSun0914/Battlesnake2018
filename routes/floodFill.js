4830d62bf879121f83dd2fab061a4761d9b9f145
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-02-27 15:04:53 -0800

    trying to add floodfill algorithm

--- routes/floodFill.js
+++ routes/floodFill.js
@@ -1,19 +1,19 @@
 var contains = require('./functions/contains.js')
-var check = false;
 
 module.exports = exports = function (mySnake, enemies, board) {
 	var openSpaces = [];
+	var check = false;
 
 	//find node that is neighbouring head that is not blocked in order to start
 
 	var head = new Point(mySnake.body[0].x, mySnake.body[0].y);
-	dfs(openSpaces, head, board.width, board.height, enemies, mySnake);
+	dfs(check, openSpaces, head, board.width, board.height, enemies, mySnake);
 
 	return openSpaces;
 
 }
 
-var dfs = function (openSpaces, node, boardWidth, boardHeight, enemies, mySnake) {
+var dfs = function (check, openSpaces, node, boardWidth, boardHeight, enemies, mySnake) {
 
 	if(!(mySnake.body[0].x == node.x && mySnake.body[0].y == node.y)) {
 		check = true;
@@ -47,16 +47,16 @@ var dfs = function (openSpaces, node, boardWidth, boardHeight, enemies, mySnake)
 	var caseThree = new Point(node.x, node.y+1);
 	var caseFour = new Point(node.x, node.y-1);
 
-	if(dfs(openSpaces, caseOne, boardWidth, boardHeight, enemies, mySnake)) {
+	if(dfs(check, openSpaces, caseOne, boardWidth, boardHeight, enemies, mySnake)) {
 		return true;
 	}
-	if(dfs(openSpaces, caseTwo, boardWidth, boardHeight, enemies, mySnake)) {
+	if(dfs(check, openSpaces, caseTwo, boardWidth, boardHeight, enemies, mySnake)) {
 		return true;
 	}
-	if(dfs(openSpaces, caseThree, boardWidth, boardHeight, enemies, mySnake)) {
+	if(dfs(check, openSpaces, caseThree, boardWidth, boardHeight, enemies, mySnake)) {
 		return true;
 	}
-	if(dfs(openSpaces, caseFour, boardWidth, boardHeight, enemies, mySnake)) {
+	if(dfs(check, openSpaces, caseFour, boardWidth, boardHeight, enemies, mySnake)) {
 		return true;
 	}
 	return false;
@@ -69,11 +69,11 @@ function Point(x, y) {
 
 var checkIfBlocked = function(node, enemies, mySnake) {
 	if(contains(mySnake.body, node.x, node.y)) {
-		return false;
+		return true;
 	}
 	for(var i = 0; i < enemies.length; i++) {
 		if(contains(enemies[i].body, node.x, node.y)) {
-			return false;
+			return true;
 		}
 	}
 	return true;
