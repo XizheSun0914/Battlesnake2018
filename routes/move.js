561536e27e54b2974c20fc1b02dec17d2ff7bed4
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-02-27 15:55:11 -0800

    trying to add floodfill algorithm

--- routes/move.js
+++ routes/move.js
@@ -25,7 +25,7 @@ module.exports = exports = function (mySnake, enemies, board) {
 	};
 
 	//edit these goals with testing
-	var goalLength = board.height*board.width/12;
+	var goalLength = board.height*board.width/8;
 	var lowHealth = board.height*board.width/8;
 
 	if(mySnake.health < lowHealth || mySnake.length < goalLength) {
