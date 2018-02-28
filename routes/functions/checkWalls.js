62ccd55eaade142e61e9dcc3531a93f14ad0ffa8
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-02-28 13:47:02 -0800

    testing astar

--- routes/functions/checkWalls.js
+++ routes/functions/checkWalls.js
@@ -8,7 +8,6 @@ var contains = require('./contains.js')
 module.exports = exports = function (mySnake, board, decision)  {
 	//top left corner
 	if(mySnake.body[0].x == 0 && mySnake.body[0].y == 0){
-		console.log("top left corner");
 		if(contains(mySnake.body, 1, 0)) {
 			decision.down += 99999;
 		} else {
@@ -17,7 +16,6 @@ module.exports = exports = function (mySnake, board, decision)  {
 	}
 	//bottom left corner
 	if(mySnake.body[0].x == 0 && mySnake.body[0].y == board.height) {
-		console.log("bottom left corner");
 		if(contains(mySnake.body, 0, board.height-1)) {
 			decision.right += 99999;
 		} else {
@@ -26,7 +24,6 @@ module.exports = exports = function (mySnake, board, decision)  {
 	}
 	//top right corner
 	if(mySnake.body[0].x == board.width && mySnake.body[0].y == 0) {
-		console.log("top right corner");
 		if(contains(mySnake.body, board.width-1, 0)) {
 			decision.down += 99999;
 		} else {
@@ -35,7 +32,6 @@ module.exports = exports = function (mySnake, board, decision)  {
 	}
 	//bottom right corner
 	if(mySnake.body[0].x == board.width && mySnake.body[0].y == board.height) {
-		console.log("bottom right corner");
 		if(contains(mySnake.body, board.width, board.height-1)) {
 			decision.left += 99999;
 		} else {
@@ -45,22 +41,18 @@ module.exports = exports = function (mySnake, board, decision)  {
 
 	//left side
 	if(mySnake.body[0].x == 0) {
-		console.log("on left wall");
 		decision.left -= 99999;
 	}
 	// right side
 	if(mySnake.body[0].x == board.width) {
-		console.log("on right wall");
 		decision.right -= 99999;
 	}
 	// top
 	if(mySnake.body[0].y == 0) {
-		console.log("on top wall");
 		decision.up -= 99999;
 	}
 	//bottom
 	if(mySnake.body[0].y == board.height) {
-		console.log("on bottom wall");
 		decision.down -= 99999;
 	}
 	return;
