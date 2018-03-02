c6f440ee15d887b27b0b3d93c1a236eb6ea52f18
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-03-02 01:56:23 -0800

    optimized floodfill

--- routes/functions/floodFill.js
+++ routes/functions/floodFill.js
@@ -60,24 +60,24 @@ var fill = function(mySnake, grid, openSpaces) {
 			openSpaces.push(temp);
 			console.log("checkpoint 4");
 
-			if(x >= 0) {
-				var left = new Point(x-1, y);
+			if(x > -1) {
+				var left = new Point((x-1), y);
 				queue.push(left);
 			}
 
-			if(x <= board.width) {
-				var right = new Point(x+1, y);
+			if(x < board.width+1) {
+				var right = new Point((x+1), y);
 				queue.push(right);
 			}
 			console.log("checkpoint 5");
 
-			if(y >= 0) {
-				var up = new Point(x, y-1);
+			if(y > -1) {
+				var up = new Point(x, (y-1));
 				queue.push(left);
 			}
 
-			if(y <= board.height) {
-				var down = new Point(x, y+1);
+			if(y < board.height+1) {
+				var down = new Point(x, (y+1));
 				queue.push(down);
 			}
 		}
