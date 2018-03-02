577e3f8f594f858e314e9a2f9bf44e77be870926
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-03-01 22:20:31 -0800

    seriously improved performance of findFood

--- routes/functions/findFood.js
+++ routes/functions/findFood.js
@@ -1,5 +1,3 @@
-//test to figure what amount of openSpace works best
-
 var aStar = require('./aStar.js')
 var floodFill = require('./floodFill.js')
 var contains = require('./contains.js')
@@ -36,65 +34,85 @@ module.exports = exports = function(mySnake, enemies, board, decision) {
 		return a[a.length-1].f - b[b.length-1].f;
 	});
 
-	//checks before returning if there is enough room for the move.
-	//makes a variable (temp) equal mySnake and push the direction we want to go onto the head
-	//rotates through all options until success or failure
+	var leftRoute = new rPoint(mySnake.body[0].x-1, mySnake.body[0].y);
+	var rightRoute = new rPoint(mySnake.body[0].x+1, mySnake.body[0].y);
+	var upRoute = new rPoint(mySnake.body[0].x, mySnake.body[0].y-1);
+	var downRoute = new rPoint(mySnake.body[0].x, mySnake.body[0].y+1);
 
-	//-----------------------------------------------------------------------
-
-	//lowers value of each decision in order of priority (if floodfill succeeds)
-	var decreaseVal = 2000;
+	var counter = 1;
 
 	while(routes.length > 0) {
+		var temp = routes.shift();
 
-		var topRoute = routes.shift();
+		if(temp[1].x == leftRoute.x && temp[1].y == leftRoute.y) {
+			leftRoute.routes.push(counter);
+			counter++;
+		}
+		if(temp[1].x == rightRoute.x && temp[1].y == rightRoute.y) {
+			rightRoute.routes.push(counter);
+			counter++;
+		}
+		if(temp[1].x == upRoute.x && temp[1].y == upRoute.y) {
+			upRoute.routes.push(counter);
+			counter++;
+		}
+		if(temp[1].x == downRoute.x && temp[1].y == downRoute.y) {
+			downRoute.routes.push(counter);
+			counter++;
+		}
+	}
 
-		//acts as if the head is one forward and tail is too
+	enoughSpace(leftRoute, mySnake, enemies, board, decision);
+	enoughSpace(rightRoute, mySnake, enemies, board, decision);
+	enoughSpace(upRoute, mySnake, enemies, board, decision);
+	enoughSpace(downRoute, mySnake, enemies, board, decision);
+
+	return;
+}
+
+//checks before returning if there is enough room for the move.
+//makes a variable (temp) equal mySnake and push the direction we want to go onto the head
+//rotates through all options until success or failure
+var enoughSpace = function(currentRoute, mySnake, enemies, board, decision) {
+	if(currentRoute.routes.length != 0) {
 		var temp = JSON.parse(JSON.stringify(mySnake));
-		temp.body.unshift(topRoute[1]);
+		temp.body.unshift(currentRoute);
 		temp.body.pop();
 
-		//checks if i can reach my tail after getting food, and no enemy heads coming into area
-		var tailReachable = aStar(board, temp, enemies, temp.body[temp.length-1]);
-		var enemiesInSpace = [];
-		for(var i = 0; i < enemies.length; i++) {
-			var checker = aStar(board, temp, enemies, enemies[i].body[0]);
-			if(checker.length > 0) {
-				enemiesInSpace.push(checker);
+		var space = floodFill(temp, enemies, board);
+		console.log("down room: " + space.length);
+
+		if(!(space > temp.length*4)) {
+			//checks if i can reach my tail after getting food, and no enemy heads coming into area
+			var tailReachable = aStar(board, temp, enemies, temp.body[temp.length-1]);
+			var enemiesInSpace = [];
+			for(var i = 0; i < enemies.length; i++) {
+				var checker = aStar(board, temp, enemies, enemies[i].body[0]);
+				if(checker.length > 0) {
+					enemiesInSpace.push(checker);
+				}
+			}
+
+			if(space.length > mySnake.length || (tailReachable.length > 0 && space.length > 1 && enemiesInSpace.length == 0) || (space.length*(4/3) > mySnake.length && mySnake.health < 15)) {
+				currentRoute.check = true;
 			}
 		}
 
-		var space = floodFill(temp, enemies, board);
-
-		console.log("room: " + space.length);
-
-		//if theres more than enough space to fit; go for it, if i can get back to tail; go for it, or if absolutely desparate for food
-		//decreases each path value following each successful path
-		if(space.length > mySnake.length || (tailReachable.length > 0 && space.length > 1 && enemiesInSpace.length == 0) || (space.length*(4/3) > mySnake.length && mySnake.health < 15)) {
-			
-			console.log("direction: " + temp.body[0].x + " " + temp.body[0].y + " passed floodfill criteria");
-
-			if(temp.body[0].x > mySnake.body[0].x) {
-				decision.right += decreaseVal;
-				decreaseVal = decreaseVal*(1/3);
-			}
-			if(temp.body[0].x < mySnake.body[0].x) {
-				decision.left += decreaseVal;
-				decreaseVal = decreaseVal*(1/3);
-			}
-			if(temp.body[0].y > mySnake.body[0].y) {
-				decision.down += decreaseVal;
-				decreaseVal = decreaseVal*(1/3);
-			}
-			if(temp.body[0].y < mySnake.body[0].y) {
-				decision.up += decreaseVal;
-				decreaseVal = decreaseVal*(1/3);
-			}
-			continue;
-		} 
-		console.log("direction: " + temp.body[0].x + " " + temp.body[0].y + " failed floodfill criteria");
+		for(var k = 0; k < currentRoute.routes.length; k++) {
+			decision.down += 2000*(Math.pow((1/3), currentRoute.routes[k]));
+		}
 	}
-
-	//returns decision with no new direction values if all food options fail
+	if(currentRoute.check = true) {
+		console.log(currentRoute.x + " " + currentRoute.y + " passed floodfill criteria");
+	} else {
+		console.log(currentRoute.x + " " + currentRoute.y + " failed floodfill criteria");
+	}
 	return;
+}
+
+function rPoint(x, y) {
+	this.x;
+	this.y;
+	this.routes = [];
+	this.check = false;
 }
