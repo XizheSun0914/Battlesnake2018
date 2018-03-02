853695776769fd28ba4b320e33afb679bec20225
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-03-02 12:52:24 -0800

    optimized floodfill

--- routes/functions/floodFill.js
+++ routes/functions/floodFill.js
@@ -35,14 +35,14 @@ module.exports = exports = function (mySnake, enemies, board) {
 	//-------------------
 
 	//start floodfill at head
-	fill(mySnake, grid, openSpaces);
+	fill(mySnake, grid, openSpaces, board);
 
 	//returns list of all coordinates in reach
 	return openSpaces;
 
 }
 
-var fill = function(mySnake, grid, openSpaces) {
+var fill = function(mySnake, grid, openSpaces, board) {
 	console.log("checkpoint 1");
 	var queue = [];
 	var head = new Point(mySnake.body[0].x, mySnake.body[0].y);
