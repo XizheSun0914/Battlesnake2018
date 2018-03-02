41dbbe353a29f41c858e431627889cda82bf4e2b
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-03-01 17:12:09 -0800

    adding new keepAlive function if trapped

--- routes/functions/aStar.js
+++ routes/functions/aStar.js
@@ -28,8 +28,8 @@ module.exports = exports = function (board, mySnake, enemies, food) {
 		//create successors
 		for(var i = -1; i <= 1; i++) {
 			for(var j = -1; j <= 1; j++) {
-				//if we cant reach, skip
-				if((i==0 && j==0) || (i != 0 && j != 0) || (!isValid(q.x+i, q.y+j, enemies, mySnake, board))) {
+				//if we cant reach, skip. unless its our goal (say we're chasing an enemy tail)
+				if((i==0 && j==0) || (i != 0 && j != 0) || (!isValid(q.x+i, q.y+j, enemies, mySnake, board) && q.x+i != food.x && q.y+j != food.y)) {
 					continue;
 				} else {
 					var successor = new aNode(q.x+i, q.y+j, q.f, q, food, enemies, mySnake);
