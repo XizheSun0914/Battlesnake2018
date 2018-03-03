e11b1c7de32ef17dcda98a054fb4bbee7c2ce42a
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-03-03 10:41:02 -0800

    testing

--- routes/functions/utilities/aStar.js
+++ routes/functions/utilities/aStar.js
@@ -37,6 +37,7 @@ module.exports = exports = function (board, mySnake, enemies, food) {
 			for(var j = -1; j <= 1; j++) {
 				var xCoord = (q.x+i);
 				var yCoord = (q.y+j);
+				console.log("parent: " + q.parent.x + " " + q.parent.y);
 				console.log(xCoord + " " + yCoord + " == " + grid[xCoord][yCoord]);
 				//if we cant reach, skip. unless its our goal (say we're chasing an enemy tail or my tail)
 				if(i==0 && j==0) {
