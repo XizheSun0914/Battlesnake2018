ae0d50e0fa8ab3026861e33cb84240bb0d9d0d10
Craig Mathieson <craigmathieson@Craigs-MacBook-Pro.local>
2018-03-02 21:29:54 -0800

    testing

--- routes/functions/utilities/aStar2.js
+++ routes/functions/utilities/aStar2.js
@@ -2,35 +2,84 @@ var buildGrid = require('./buildGrid.js')
 var contains = require('./contains.js')
 
 module.exports = exports = function (board, mySnake, enemies, food) {
-	var closedSpace = []
-	for(var i = 0; i < board.width; i++) {
-		for(var j = 0; j < board.height; j++) {
-			if(!isValid(i, j, board, grid)) {
-				console.log(i + " " + j + "not empty here");
-				var temp = new aNode(i, j, 10, null, food, enemies, mySnake, grid);
-				closedSpace.push(temp);
+	var closedList = [];
+	var openList = [];
+	var grid = buildGrid(mySnake, board, enemies);
+
+	console.log(grid);
+
+	var head = new aNode(mySnake.body[0].x, mySnake.body[0].y, -1, null, food, enemies, mySnake);
+	openList.push(head);
+
+	while(openList.length != 0) {
+
+		//IMPORVE SPEED HERE BY ADDING TO A SORTED LIST OR HEAP
+		openList.sort(function(a,b) {	//sort openList based on total cost
+			return a.f - b.f;
+		});
+
+		var q = openList.shift();
+		closedList.push(q);
+		console.log(q.x + " " + q.y);
+
+		//if at destination, build route and finish
+		if(q.x == food.x && q.y == food.y) {
+			var temp = finishRoute(q, head);
+			return temp;
+		}
+		console.log("checkpoint 1");
+
+		var successors = [];
+
+		//create successors
+		for(var i = -1; i <= 1; i++) {
+			for(var j = -1; j <= 1; j++) {
+				console.log((q.x+i) + " " + (q.y+j));
+				//if we cant reach, skip. unless its our goal (say we're chasing an enemy tail or my tail)
+				if((i==0 && j==0) || (i != 0 && j != 0) || (!grid[q.x+i][q.y+j]) && !(q.x+i == food.x && q.y+j == food.y))) {
+					console.log("failure: " + (q.x+i) + " " + (q.y+j));
+					continue;
+				} else {
+					console.log("success: " + successor.x + " " + successor.y);
+					var successor = new aNode(q.x+i, q.y+j, q.f, q, food, enemies, mySnake);
+					successors.push(successor);
+				}
 			}
 		}
-	}
-	console.log(closedSpace);
-	return closedSpace;
-}
 
-//checks if node is already covered by enemy or 
-//friendly snake or if outside board
+		console.log("checkpoint 2");
 
-function isValid(x, y, board, grid) {
-	if(grid[x][y] == 1) {
-		return false;
-	}
-	if(grid[x][y] == 2) {
-		return false;
-	}
+		for(var i = 0; i < successors.length; i++) {
+			//if on closedList, ignore
+			//MAYBE USE GRID SPACE = 3 FOR CLOSEDLIST
+			if(contains(closedList, successors[i].x, successors[i].y)) {
+				continue;
+			}
 
-	if (x <= board.width && x >= 0 && y <= board.height && y >= 0) {
-		return true;
-	} else {
-		return false;
+			//MAYBE USE GRID SPACE = 4 FOR OPENLIST
+			//if not in openList, add it
+			if(!contains(openList, successors[i].x, successors[i].y)) {
+				openList.push(successors[i]);
+				continue;
+			}
+
+			//MAYBE USE GRID SPACE = 4 FOR OPENLIST
+			//if openList has same nodes cheaper than successor[i]: continue, else: push to openList
+			if(contains(openList, successors[i].x, successors[i].y)) {
+				var check = false;
+				for(var j = 0; j < openList.length; j++) {
+					if(openList[j].x == successors[i].x && openList[j].y == successors[i].y && openList[j].f < successors[i].f) {
+						check = true;
+					}
+				}
+				if(check) {
+					continue;
+				} else {
+					openList.push(successors[i]);
+				}
+			}
+		}
+		console.log("checkpoint 3");
 	}
 }
 
